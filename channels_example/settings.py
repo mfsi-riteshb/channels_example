9fde5985b16b1e60ea8df26d559c98ad9ccc4012
mfsi-riteshb <riteshb@mindfiresolutions.com>
2017-09-25 22:11:20 +0530

    Fix

--- channels_example/settings.py
+++ channels_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#=trk_-t6!zr+lv(nt45@g()^upc$6$m=ri&s4a%7^l3epnaw1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://channels-example-demo.herokuapp.com/']
+ALLOWED_HOSTS = ['channels-example-demo.herokuapp.com']
 
 
 # Application definition
