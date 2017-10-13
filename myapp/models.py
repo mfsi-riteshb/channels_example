ef38488efafddec5fffba1ad122e862dd664a2ce
fahad-gcet <fms525@outlook.com>
2017-10-13 13:39:45 +0530

    add image to screen

--- myapp/models.py
+++ myapp/models.py
@@ -25,6 +25,9 @@ class Room(models.Model):
     number_of_current_user = models.IntegerField(default=0)
     number_of_slides = models.IntegerField(default=1)
 
+def get_upload_to(instance, filename):
+    return 'screens/%d/%s' % (instance.room, filename)
+
 
 class Screen(models.Model):
     """Screen model."""
@@ -37,6 +40,7 @@ class Screen(models.Model):
 
     content = RichTextUploadingField(max_length=50000)
     room = models.ForeignKey(Room, related_name='screens')
+    image = models.FileField(upload_to=get_upload_to)
 
 
 class RoomActiveUser(models.Model):
